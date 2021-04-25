e9af758877cc122dd01d29a134bcacd1cff59f9e
kongFu-FBI <shifulong7@gmail.com>
2021-04-25 20:17:28 +0800

    git push

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import json
-filname = 'numbers.json'
-try:
-        with open(filname) as f:
-                username = json.load(f)
-except FileNotFoundError:
-        username = input("name? ")
-        with open(filname,'w') as f:
-                json.dump(username,f)
-                print("two")
-else:
-        print(f"wlecom ，{username}")
+import matplotlib.pyplot as plt
+from pylab import *
+mpl.rcParams['font.sans-serif'] = ['SimHei']
+squares = [1, 4, 9, 16, 25]
+fig, ax = plt.subplots()
+ax.set_title("平方数", fontsize=24)
+ax.set_xlabel("值", fontsize=14)
+ax.set_ylabel("值的平方", fontsize=14)
+ax.tick_params(axis='both', labelsize=14)
+ax.plot(squares)
 
+plt.show()
